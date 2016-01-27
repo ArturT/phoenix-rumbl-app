434719c7ef3e2a676ca8e734e6fccd32a706f7b0
ArturT <artur.trzop@lunarlogic.io>
2016-01-27 16:00:28 +0100

    Add Auth Plug and protect index action

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -20,12 +20,28 @@ defmodule Rumbl.UserController do
   end
 
   def index(conn, _params) do
-    users = Repo.all(Rumbl.User)
-    render conn, "index.html", users: users
+    case authenticate(conn) do
+      %Plug.Conn{halted: true} = conn ->
+        conn
+      conn ->
+        users = Repo.all(Rumbl.User)
+        render conn, "index.html", users: users
+    end
   end
 
   def show(conn, %{"id" => id}) do
     user = Repo.get(Rumbl.User, id)
     render conn, "show.html", user: user
+  end
+
+  defp authenticate(conn) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page.")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
+    end
   end
 end
