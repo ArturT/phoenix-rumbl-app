bfb873e4a66ca68802ecaf301d3bf868cfbf281a
ArturT <artur.trzop@lunarlogic.io>
2016-01-27 16:11:51 +0100

    Login user after creating account

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -10,4 +10,13 @@ defmodule Rumbl.Auth do
     user = user_id && repo.get(Rumbl.User, user_id)
     assign(conn, :current_user, user)
   end
+
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+    # The step is extremely important and it protects us from session fixation attacks.
+    # It tells Plug to send the session cookie back to the client with a different identifier, in case an attacker knew, by any chance, the previous one.
+    |> configure_session(renew: true)
+  end
 end
