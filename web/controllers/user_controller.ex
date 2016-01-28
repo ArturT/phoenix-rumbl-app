302a1dafd386e02f7240263507040a5bbf019038
ArturT <artur.trzop@lunarlogic.io>
2016-01-28 13:27:06 +0100

    Extract authenticate and rename it to authenticate_user. Import it for controller and routes

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -2,7 +2,7 @@ defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
   alias Rumbl.User
 
-  plug :authenticate when action in [:index, :show]
+  plug :authenticate_user when action in [:index, :show]
 
   def new(conn, _params) do
     changeset = User.changeset(%User{}, :empty)
@@ -30,16 +30,5 @@ defmodule Rumbl.UserController do
   def show(conn, %{"id" => id}) do
     user = Repo.get(Rumbl.User, id)
     render conn, "show.html", user: user
-  end
-
-  defp authenticate(conn, _opts) do
-    if conn.assigns.current_user do
-      conn
-    else
-      conn
-      |> put_flash(:error, "You must be logged in to access that page.")
-      |> redirect(to: page_path(conn, :index))
-      |> halt()
-    end
   end
 end
