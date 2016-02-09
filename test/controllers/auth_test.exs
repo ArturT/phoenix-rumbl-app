68e2ad3f932a866d759d75d5131f67b80efcef79
ArturT <artur.trzop@lunarlogic.io>
2016-02-09 21:11:05 +0100

    We added a setup block which calls bypass_through, passing our router and the :browser pipeline to invoke. Then we perform a request with get which will access the endpoint and stop at the browser pipeline, as requested. This will give us all the requirements for a plug with a valid session and flash message support. Next, we pulled the conn from the context passed to the test macro and used our bypassed conn as the base for our test blocks.

---
+++ test/controllers/auth_test.exs
@@ -0,0 +1,27 @@
+defmodule Rumbl.AuthTest do
+  use Rumbl.ConnCase
+  alias Rumbl.Auth
+
+  setup %{conn: conn} do
+    conn =
+      conn
+      |> bypass_through(Rumbl.Router, :browser)
+      |> get("/")
+
+    {:ok, %{conn: conn}}
+  end
+
+  test "authenticate_user halts when no current_user exists", %{conn: conn} do
+    conn = Auth.authenticate_user(conn, [])
+    assert conn.halted
+  end
+
+  test "authenticate_user continues when the current_user exists", %{conn: conn} do
+    conn =
+      conn
+      |> assign(:current_user, %Rumbl.User{})
+      |> Auth.authenticate_user([])
+
+    refute conn.halted
+  end
+end
