50b10525675dfe7b7fd0e87fcd58e5ca84102a0c
ArturT <artur.trzop@lunarlogic.io>
2016-02-09 20:08:46 +0100

    add test for create video and when errors occur

--- test/controllers/video_controller_test.exs
+++ test/controllers/video_controller_test.exs
@@ -1,5 +1,10 @@
 defmodule Rumbl.VideoControllerTest do
   use Rumbl.ConnCase
+  alias Rumbl.Video
+  @valid_attrs %{url: "http://youtu.be", title: "vid", description: "a vid"}
+  @invalid_attrs %{title: "invalid"}
+
+  defp video_count(query), do: Repo.one(from v in query, select: count(v.id))
 
   test "requires user authentication on all actions", %{conn: conn} do
     Enum.each([
@@ -38,5 +43,22 @@ defmodule Rumbl.VideoControllerTest do
     assert html_response(conn, 200) =~ ~r/Listing videos/
     assert String.contains?(conn.resp_body, user_video.title)
     refute String.contains?(conn.resp_body, other_video.title)
+  end
+
+  @tag login_as: "max"
+  test "creates user video and redirects", %{conn: conn, user: user} do
+    conn = post conn, video_path(conn, :create), video: @valid_attrs
+
+    assert redirected_to(conn) == video_path(conn, :index)
+    assert Repo.get_by!(Video, @valid_attrs).user_id == user.id
+  end
+
+  @tag login_as: "max"
+  test "does not create video and renders errors when invalid", %{conn: conn} do
+    count_before = video_count(Video)
+    conn = post conn, video_path(conn, :create), video: @invalid_attrs
+
+    assert html_response(conn, 200) =~ "check the errors"
+    assert video_count(Video) == count_before
   end
 end
