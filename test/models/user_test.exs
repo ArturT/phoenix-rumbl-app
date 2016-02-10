23278c974b3ebd4bf08c270785c620cd133d8ac1
ArturT <artur.trzop@lunarlogic.io>
2016-02-10 16:49:30 +0100

    add tests for user model

---
+++ test/models/user_test.exs
@@ -0,0 +1,25 @@
+defmodule Rumbl.UserTest do
+  use Rumbl.ModelCase, async: true
+  alias Rumbl.User
+
+  @valid_attrs %{name: "A User", username: "eva", password: "secret"}
+  @invalid_attrs %{}
+
+  test "changeset with valid attributes" do
+    changeset = User.changeset(%User{}, @valid_attrs)
+
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = User.changeset(%User{}, @invalid_attrs)
+
+    refute changeset.valid?
+  end
+
+  test "changeset does not accept long usernames" do
+    attrs = Map.put(@valid_attrs, :username, String.duplicate("a", 30))
+
+    assert username: {"should be at most %{count} characters", [count: 20]} in errors_on(%User{}, attrs)
+  end
+end
