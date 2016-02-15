896d0718aa6aaada781f55db6699c0bec5a6df36
ArturT <artur.trzop@lunarlogic.io>
2016-02-15 20:54:28 +0100

    Add slug to watch video
    iex(1)> video = %Rumbl.Video{id: 1, slug: "hello"}
    %Rumbl.Video{__meta__: #Ecto.Schema.Metadata<:built>,
     category: #Ecto.Association.NotLoaded<association :category is not loaded>,
     category_id: nil, description: nil, id: 1, inserted_at: nil, slug: "hello",
     title: nil, updated_at: nil, url: nil,
     user: #Ecto.Association.NotLoaded<association :user is not loaded>,
     user_id: nil}
    
    iex(2)> Rumbl.Router.Helpers.watch_path(%URI{}, :show, video)
    "/watch/1-hello"
    
    iex(3)> url = URI.parse("http://example.com/prefix")
    %URI{authority: "example.com", fragment: nil, host: "example.com",
     path: "/prefix", port: 80, query: nil, scheme: "http", userinfo: nil}
    
    iex(4)> Rumbl.Router.Helpers.watch_path(url, :show, video)
    "/prefix/watch/1-hello"
    
    iex(5)> Rumbl.Router.Helpers.watch_url(url, :show, video)
    "http://example.com/prefix/watch/1-hello"
    
    // How to build url which can be used in email templates etc
    
    iex(6)> url = Rumbl.Endpoint.struct_url
    %URI{authority: nil, fragment: nil, host: "localhost", path: nil, port: 4000,
     query: nil, scheme: "http", userinfo: nil}
    
    iex(7)> Rumbl.Router.Helpers.watch_url(url, :show, video)
    "http://localhost:4000/watch/1-hello"

--- web/models/video.ex
+++ web/models/video.ex
@@ -5,6 +5,7 @@ defmodule Rumbl.Video do
     field :url, :string
     field :title, :string
     field :description, :string
+    field :slug, :string
     belongs_to :user, Rumbl.User
     belongs_to :category, Rumbl.Category
 
@@ -23,6 +24,27 @@ defmodule Rumbl.Video do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
+    |> slugify_title()
     |> assoc_constraint(:category)
+  end
+
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      changeset
+    end
+  end
+
+  defp slugify(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/, "-")
+  end
+end
+
+defimpl Phoenix.Param, for: Rumbl.Video do
+  def to_param(%{slug: slug, id: id}) do
+    "#{id}-#{slug}"
   end
 end
