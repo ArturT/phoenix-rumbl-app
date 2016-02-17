6775a4b9a0cb3a6b5a311bbc0b221c62f7a885b3
ArturT <artur.trzop@lunarlogic.io>
2016-02-17 21:44:05 +0100

    Persist annotations in DB

--- web/channels/video_channel.ex
+++ web/channels/video_channel.ex
@@ -5,13 +5,28 @@ defmodule Rumbl.VideoChannel do
     {:ok, assign(socket, :video_id, video_id)}
   end
 
-  def handle_in("new_annotation", params, socket) do
-    broadcast! socket, "new_annotation", %{
-      user: %{username: "anon"},
-      body: params["body"],
-      at: params["at"]
-    }
+  def handle_in(event, params, socket) do
+    user = Repo.get(Rumbl.User, socket.assigns.user_id)
+    handle_in(event, params, user, socket)
+  end
 
-    {:reply, :ok, socket}
+  def handle_in("new_annotation", params, user, socket) do
+    video_id = String.to_integer(socket.assigns.video_id)
+    changeset =
+      user
+      |> build_assoc(:annotations, video_id: video_id)
+      |> Rumbl.Annotation.changeset(params)
+
+    case Repo.insert(changeset) do
+      {:ok, annotation} ->
+        broadcast! socket, "new_annotation", %{
+          user: Rumbl.UserView.render("user.json", %{user: user}),
+          body: annotation.body,
+          at: annotation.at
+        }
+        {:reply, :ok, socket}
+      {:error, changeset} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
   end
 end
