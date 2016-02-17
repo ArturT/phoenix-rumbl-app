a064c067b3550fb8f24eb8a92dd12b929fff530e
ArturT <artur.trzop@lunarlogic.io>
2016-02-17 21:08:16 +0100

    Add authentication for channel

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -13,9 +13,9 @@ defmodule Rumbl.Auth do
 
     cond do
       user = conn.assigns[:current_user] ->
-        assign(conn, :current_user, user)
+        put_current_user(conn, user)
       user = user_id && repo.get(Rumbl.User, user_id) ->
-        assign(conn, :current_user, user)
+        put_current_user(conn, user)
       true ->
         assign(conn, :current_user, nil)
     end
@@ -24,6 +24,7 @@ defmodule Rumbl.Auth do
   def login(conn, user) do
     conn
     |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     # The step is extremely important and it protects us from session fixation attacks.
     # It tells Plug to send the session cookie back to the client with a different identifier, in case an attacker knew, by any chance, the previous one.
@@ -64,5 +65,13 @@ defmodule Rumbl.Auth do
       |> redirect(to: Helpers.page_path(conn, :index))
       |> halt()
     end
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 end
