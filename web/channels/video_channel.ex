5605c8f7e3fc5971fa568c5dd4cf259be11b4ff3
ArturT <artur.trzop@lunarlogic.io>
2016-02-17 20:44:42 +0100

    handle annotation on server side and broadcast it.
    IMPORTANT: Don’t broadcast raw message payload to all clients. We want
    to control the payload for security reason.

--- web/channels/video_channel.ex
+++ web/channels/video_channel.ex
@@ -4,4 +4,14 @@ defmodule Rumbl.VideoChannel do
   def join("video:" <> video_id, _params, socket) do
     {:ok, assign(socket, :video_id, video_id)}
   end
+
+  def handle_in("new_annotation", params, socket) do
+    broadcast! socket, "new_annotation", %{
+      user: %{username: "anon"},
+      body: params["body"],
+      at: params["at"]
+    }
+
+    {:reply, :ok, socket}
+  end
 end
