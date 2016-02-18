3b85e27420e5aa0574e1330ad9f10c4135cde527
ArturT <artur.trzop@lunarlogic.io>
2016-02-18 17:13:51 +0100

    Add Counter as worker

--- lib/counter.ex
+++ lib/counter.ex
@@ -14,6 +14,7 @@ defmodule Rumbl.Counter do
   end
 
   def init(initial_val) do
+    Process.send_after(self, :tick, 1000)
     {:ok, initial_val}
   end
 
@@ -27,5 +28,11 @@ defmodule Rumbl.Counter do
 
   def handle_call(:val, _from, val) do
     {:reply, val, val}
+  end
+
+  def handle_info(:tick, val) do
+    IO.puts "tick #{val}"
+    Process.send_after(self, :tick, 1000)
+    {:noreply, val - 1}
   end
 end
--- lib/rumbl.ex
+++ lib/rumbl.ex
@@ -13,6 +13,7 @@ defmodule Rumbl do
       supervisor(Rumbl.Repo, []),
       # Here you could define other workers and supervisors as children
       # worker(Rumbl.Worker, [arg1, arg2, arg3]),
+      worker(Rumbl.Counter, [5]), # new counter worker
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
